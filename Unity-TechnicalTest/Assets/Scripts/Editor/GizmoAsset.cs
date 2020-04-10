9bbaa324fbf0f10368ef66053215b6c73347c16b
Roppii <ng.roche7@gmail.com>
2020-04-11 01:16:54 +0200

    Import initial project

---
+++ Unity-TechnicalTest/Assets/Scripts/Editor/GizmoAsset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace technical.test.editor
+{
+    [CreateAssetMenu(fileName = "Gizmo Asset", menuName = "Custom/Create Information Gizmo Asset")]
+    public class GizmoAsset : ScriptableObject
+    {
+        [SerializeField]
+        private InformationGizmo[] _gizmos = default;
+    }
+
+}
---
+++ Unity-TechnicalTest/Assets/Scripts/Editor/GizmoInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace technical.test.editor
+{
+
+    [Serializable]
+    public struct InformationGizmo
+    {
+        public string Name;   
+        public Vector3 Position;
+
+        public InformationGizmo(string name, Vector3 position)
+        {
+            Name = name;
+            Position = position;
+        }
+    }
+
+}

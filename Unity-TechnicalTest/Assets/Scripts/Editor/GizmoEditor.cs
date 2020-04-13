76f65fb8f51b3de0ac8bcdc3432c7c90cb8e3fb9
Roppii <ng.roche7@gmail.com>
2020-04-13 23:38:43 +0200

    delivery-v1.0

--- Unity-TechnicalTest/Assets/Scripts/Editor/GizmoEditor.cs
+++ Unity-TechnicalTest/Assets/Scripts/Editor/GizmoEditor.cs
@@ -3,61 +3,227 @@ using UnityEditor;
 using technical.test.editor;
 
 public class GizmoEditor : EditorWindow {
-    
-    private GizmoAsset gizmoAsset;
 
+    #region Variables
+    // References
+    private static GizmoEditor _instance = null;
+    private static SerializedObject gizmoAsset;
+    private static SerializedProperty gizmos;
+    private GizmoAsset gizmoAssetReference;
+    
+    // GUI Management
+    private bool[] gizmoSelectedState;
+    private Vector3[] gizmoInitialPos;
+    private static bool gizmoPosInitialized = false;
+    private bool resetIsClicked = false;
+    private bool deleteIsClicked = false;
+    #endregion
+    
     [MenuItem("Window/Custom/Informative Gizmo")]
     public static void ShowWindow() {
-        GetWindow<GizmoEditor>("Gizmo Editor");
-        GetWindow<GizmoEditor>().GetSelectedAsset();
+        _instance = GetWindow<GizmoEditor>("Gizmo Editor");
+        _instance.GetSelectedAsset();
     }
 
     /* We want to know the selected GizmoAsset in order to update
-     * the content of our custom Gizmo Editor Window accordingly */
+     * the content of our custom Gizmo Editor Window accordingly. */
     void GetSelectedAsset() {
         Object[] selection = Selection.GetFiltered(typeof(GizmoAsset), SelectionMode.Assets);
-        if (selection.Length > 0) {
-            if (selection[0] == null)
-                return;
 
-            gizmoAsset = (GizmoAsset)selection[0];
+        if (selection.Length > 0) {
+            if (selection[0] == null) {
+                return;
+            }
+
+            gizmoAssetReference = (GizmoAsset)selection[0];
+
+            // This will allow us to access the selected GizmoAsset's InformationGizmo.
+            gizmoAsset = new SerializedObject(gizmoAssetReference);
+            gizmos = gizmoAsset.FindProperty("_gizmos");
+
+            // We will need this to know which gizmo is currently selected.
+            gizmoSelectedState = new bool[gizmos.arraySize];
+
+            if (!gizmoPosInitialized) {
+                /* We want to save the gizmos' initial positions so that we can reset them later on if needed.
+                 * Thus, we want their positions to be initialized only once.*/
+                gizmoInitialPos = new Vector3[gizmos.arraySize];
+                for (int i = 0; i < gizmos.arraySize; i++) {
+                    gizmoInitialPos[i] = gizmos.GetArrayElementAtIndex(i).FindPropertyRelative("Position").vector3Value;
+                }
+                gizmoPosInitialized = true;
+            }
+            
+
         }
+
     }
 
+    void OnSelectionChange() {
+        GetSelectedAsset();
+        Repaint();
+    }
+
+    #region GUI Events
     private void OnGUI() {
         
-        if (gizmoAsset == null) {
+        if (gizmoAssetReference == null) {
             return;
         }
-
-        /* I am using SerializedObject and SerializedProperty instead
-         * of putting a getter and a setter in the script GizmoAsset
-         * to simplify property modifications */
-        SerializedObject serializedObject = new SerializedObject(gizmoAsset);
-        SerializedProperty gizmos = serializedObject.FindProperty("_gizmos");
         
         EditorGUILayout.BeginVertical();
-
+        
         EditorGUILayout.BeginHorizontal();
         EditorGUILayout.LabelField("Text");
         EditorGUILayout.LabelField("Position");
         EditorGUILayout.EndHorizontal();
-        
-        for (int i = 0; i < gizmos.arraySize; i++) {
 
-            SerializedProperty gizmoName = gizmos.GetArrayElementAtIndex(i).FindPropertyRelative("Name");
-            SerializedProperty gizmoPosition = gizmos.GetArrayElementAtIndex(i).FindPropertyRelative("Position");
+        if (gizmos != null) {
+            for (int i = 0; i < gizmos.arraySize; i++) {
+                
+                SerializedProperty gizmoName = gizmos.GetArrayElementAtIndex(i).FindPropertyRelative("Name");
+                SerializedProperty gizmoPosition = gizmos.GetArrayElementAtIndex(i).FindPropertyRelative("Position");
+                
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(gizmoName, GUIContent.none);
+                EditorGUILayout.PropertyField(gizmoPosition, GUIContent.none);
+                if (GUILayout.Button("Edit")) {
+                    Edit(i);
+                }
+                EditorGUILayout.EndHorizontal();
+            }
 
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PropertyField(gizmoName, GUIContent.none);
-            EditorGUILayout.PropertyField(gizmoPosition, GUIContent.none); 
-            EditorGUILayout.EndHorizontal();
+            gizmoAsset.ApplyModifiedProperties();
         }
 
         EditorGUILayout.EndVertical();
 
-        serializedObject.ApplyModifiedProperties();
+        // We want the editor window to update whenever we make gizmo modifications.
+        Repaint();
+    }
+    
+    void OnEnable() {
+        // Remove delegate listener if it has previously been assigned.
+        SceneView.onSceneGUIDelegate -= OnSceneGUI;
+
+        // Add (or re-add) the delegate.
+        SceneView.onSceneGUIDelegate += OnSceneGUI;
     }
 
-    void OnSelectionChange() { GetSelectedAsset(); Repaint(); }
+    void OnDestroy() {
+        /* When the window is destroyed, remove the delegate
+         * so that it will no longer do any drawing.*/
+        SceneView.onSceneGUIDelegate -= OnSceneGUI;
+    }
+
+    void OnSceneGUI(SceneView sceneView) {
+
+        if ((gizmoAssetReference == null) || (gizmos ==  null)) {
+            return;
+        }
+
+        GUIStyle labelStyle = new GUIStyle { fontStyle = FontStyle.Bold };
+
+        for (int i = 0; i < gizmos.arraySize; i++) {
+            
+            SerializedProperty gizmoName = gizmos.GetArrayElementAtIndex(i).FindPropertyRelative("Name");
+            SerializedProperty gizmoPosition = gizmos.GetArrayElementAtIndex(i).FindPropertyRelative("Position");
+
+            // Display gizmos
+
+            Handles.color = Color.white;
+            Handles.SphereHandleCap(i, gizmoPosition.vector3Value, Quaternion.identity, 10f, EventType.Repaint);
+
+            Handles.color = Color.black;
+            Handles.Label(gizmoPosition.vector3Value + new Vector3(0, 15, 0), gizmoName.stringValue, labelStyle);
+            Handles.DrawLine(gizmoPosition.vector3Value, gizmoPosition.vector3Value + new Vector3(0, 10, 0));
+            
+            // Move selected gizmo
+
+            #region Alternative gizmo selection method
+            /* Comment the Handles.SphereHandleCap line above and uncomment the code below
+             *to make spheres selectable directly via Scene View*/
+
+            //if (Handles.Button(gizmoPosition.vector3Value, Quaternion.identity, 10f, 3f, Handles.SphereHandleCap)) {
+            //    for (int j = 0; j < gizmoSelectedState.Length; j++) {
+            //        gizmoSelectedState[j] = false;
+            //    }
+
+            //    gizmoSelectedState[i] = true;
+            //}
+            #endregion
+
+
+            if (gizmoSelectedState[i]) {
+                gizmoPosition.vector3Value = Handles.PositionHandle(gizmoPosition.vector3Value, Quaternion.identity);
+
+                #region Generic menu to reset or delete gizmo
+                
+                if ((Event.current.type == EventType.MouseUp) && (Event.current.button == 1)) {
+                    GenericMenu menu = new GenericMenu();
+
+                    menu.AddItem(new GUIContent("Reset Position"), false, ContextCallback, "reset");
+                    menu.AddItem(new GUIContent("Delete Gizmo"), false, ContextCallback, "delete");
+                    menu.ShowAsContext();
+
+                    Event.current.Use();
+                }
+
+                if (resetIsClicked) {
+                    gizmoPosition.vector3Value = new Vector3(gizmoInitialPos[i].x, gizmoInitialPos[i].y, gizmoInitialPos[i].z);
+                }
+                
+                resetIsClicked = false;
+
+                if (deleteIsClicked) {
+                    gizmos.DeleteArrayElementAtIndex(i);
+                }
+
+                deleteIsClicked = false;
+                #endregion
+            }
+            
+        }
+
+        gizmoAsset.ApplyModifiedProperties();
+    }
+    #endregion
+
+    #region Functions
+
+    /// <summary>
+    /// Allows us to select a gizmo.
+    /// </summary>
+    /// <param name="i">Represents the selected state of the gizmo at index i in gizmos.</param>
+    private void Edit(int i) {
+        for (int j = 0; j < gizmoSelectedState.Length; j++) {
+            gizmoSelectedState[j] = false;
+        }
+        gizmoSelectedState[i] = true;
+
+        if (SceneView.sceneViews.Count > 0) {
+            SceneView sceneView = (SceneView)SceneView.sceneViews[0];
+            sceneView.Repaint();
+        }
+    }
+
+    /// <summary>
+    /// The function called upon when a generic menu button is clicked.
+    /// </summary>
+    /// <param name="obj">Allows us to know which button is clicked.</param>
+    private void ContextCallback(object obj) {
+        string str = obj.ToString();
+        switch (str) {
+            case ("reset"):
+                resetIsClicked = true;
+                break;
+
+            case ("delete"):
+                deleteIsClicked = true;
+                break;
+
+            default: break;
+        }
+    }
+    #endregion
 }
--- Unity-TechnicalTest/README.md
+++ Unity-TechnicalTest/README.md
@@ -1,1 +1,42 @@
 # InformationGizmo
+
+/!\ The delivery commit is on the develop branch and is not merged with the master branch.
+
+ASSUMPTIONS
+
+- I assume that existing files in the initial project (GizmoAsset.cs and GizmoInformation.cs)
+should not be modified.
+
+Requirement A.
+- I assume that the custom Gizmo Editor window works like the inspector :
+	* it won't show anything unless we click on a GizmoAsset scriptable object.
+	* it will automatically show the corresponding contents of the selected GizmoAsset scriptable object
+	  when opening the window via custom inspector.
+
+Requirement C.
+- I assume that the gizmo spheres are only visible when the editor window is open, and they disappear
+  as soon as the editor window is closed.
+
+Requirement D.
+- I assume that we can only select a gizmo through clicking on the button edit. To allow direct selection
+  via Scene View, cf. GizmoEditor.cs
+
+Requirement G.
+- I assume that I can reset or delete a gizmo only when it is selected.
+
+COMMENTS
+
+Seeing as it is my first time working on editor scripting, I feel very proud to have respected almost all
+of the requirements despite having some complications. I was unable to finish the last requirement (undoing
+changes using ctrl z).
+
+I mainly struggled on :
+- understanding about how certain things work such as SceneView, Handles, OnSceneGUI in a short amount
+of time.
+- deciding on how to approach a certain problem : for example, I had to understand how Gizmo/Handle Controls
+work and figure out if they would be useful in making a gizmo interactable.
+- making the gizmos selectable, as well as rendering them in the beginning, as I did not know of
+the existence of Handles in Unity.
+- undoing the changes when pressing ctrl z. I tried using the Undo class but it doesn't seem to work on
+the editor window. Instead, it seems to affect other viewports such as the Inspector window.
+- optimizing the code because I prioritized meeting all the requirements first.
